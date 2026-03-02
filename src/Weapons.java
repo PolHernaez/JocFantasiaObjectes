13c11724e7d102dcd00f3bb6793690a4b2247a08
PolHernaez <polhernaez@gmail.com>
2026-03-02 17:56:59 +0100

    canvis dilluns

--- src/Weapons.java
+++ src/Weapons.java
@@ -1,4 +1,51 @@
 package src;
+
 public class Weapons {
+    private String type; //tipus
+    private int damage; //dany
+    private boolean magicPhysical; //magic o fisic
+
+    public Weapons(String type, int damage, boolean magicPhysical) {
+        this.type = type;
+        this.damage = damage;
+        this.magicPhysical = magicPhysical;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public void setMagicPhysical(boolean magicPhysical) {
+        this.magicPhysical = magicPhysical;
+    }
+
+    public boolean getMagicPhysical() {
+        return this.magicPhysical;
+    }
+
     
+    public String toString() {
+        String info;
+             String tipusAtac;
+        
+        if (this.magicPhysical) {
+            tipusAtac = "Màgica";
+        } else {
+            tipusAtac = "Física";
+        }
+        info =  "Tipus: " + this.type + " Dany: " + this.damage + " Màgica/Física: " + tipusAtac ;
+        return info;
+    }
 }
--- src/game.java
+++ src/game.java
@@ -1,4 +1,42 @@
 package src;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class game {
-    
+    public static void main(String[] args) {
+        game p = new game();
+        p.principal();
+    }
+    public void principal(){
+        ArrayList<Characters> character = new ArrayList<Characters>();
+        crearPJ(character);
+
+    }
+    private void crearPJ(ArrayList<Characters> character) {
+      
+    }
+    Scanner sc = new Scanner(System.in);
+   
+    public int llegirInt(String m) {
+        int text = 0;
+        boolean error = false;
+        do {
+            try {
+                System.out.println(m);
+                text = sc.nextInt();
+                error = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Error, escriu numeros");
+                error = true;
+                sc.next();
+            } catch (Exception e) {
+                System.out.println("Error desconegut, torna");
+                error = true;
+                sc.next();
+            }
+        } while (error);
+        return text;
+    }
 }
